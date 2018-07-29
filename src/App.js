935dd4ed0bf9b6ab7123201fb3646777bbcb1c03
Drew Cain <drew@simpli.fi>
2018-07-28 20:04:06 -0500

    switched to use the Router and fixed import problem with firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import firebase from '@firebase/app'
 import React, { Component } from 'react'
-import { SafeAreaView } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import firebase from 'firebase'
 import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
-import LoginForm from './components/LoginForm'
+import Router from './Router'
 
 class App extends Component {
+
   componentWillMount() {
     const config = {
       apiKey: 'AIzaSyC2nduB3z2BePVfgMkaE4wLdiZ_X2cNIsk',
@@ -19,13 +19,12 @@ class App extends Component {
     }
     firebase.initializeApp(config)
   }
+
   render() {
     const store = createStore(reducers, { /* initial state mainly for SSR */ }, applyMiddleware(ReduxThunk))
     return (
       <Provider store={store}>
-        <SafeAreaView>
-          <LoginForm />
-        </SafeAreaView>
+        <Router />
       </Provider>
     )
   }
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import firebase from '@firebase/app'
+import '@firebase/auth'
 import * as types from './types'
 
 export const emailChanged = (text) => ({
