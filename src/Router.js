15056f6fb699b03aedc1ae93f79aa80fb9ff4628
Drew Cain <drew@simpli.fi>
2018-07-28 20:39:43 -0500

    added EmployeeCreate scene

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import { Actions, Router, Scene } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => (
   <Router>
-    <Scene key="root">
-      <Scene key="login" component={LoginForm} title="Login" initial />
-      <Scene key="employeeList" component={EmployeeList} title="Employees" />
+    <Scene key="root" hideNavBar>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Login" initial />
+      </Scene>
+      <Scene key="main">
+        <Scene
+          component={EmployeeList}
+          initial
+          key="employeeList"
+          onRight={() => { Actions.employeeCreate() }}
+          rightTitle="Add"
+          title="Employees"
+        />
+        <Scene
+          component={EmployeeCreate}
+          key="employeeCreate"
+          title="Create Employee"
+        />
+      </Scene>
     </Scene>
   </Router>
 )
--- src/actions/index.js
+++ src/actions/index.js
@@ -34,5 +34,5 @@ const loginUserSuccess = (dispatch, user) => {
     type: types.LOGIN_USER_SUCCESS,
     payload: user
   })
-  Actions.employeeList()
+  Actions.main()
 }
